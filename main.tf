f52a8ed7bf743f7bbff20e1c9af065fd2439895f
Darren Tsung <darren@onesignal.com>
2019-11-03 15:43:21 -0800

    Use quoted variables so vscode extension works properly

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "digitalocean" {
   version = "~> 1.6"
-  token   = var.do_token
+  token   = "${var.do_token}"
 }
 
 resource "digitalocean_droplet" "main" {
@@ -8,7 +8,7 @@ resource "digitalocean_droplet" "main" {
   image    = "ubuntu-18-04-x64"
   region   = "sfo2"
   size     = "s-1vcpu-1gb"
-  ssh_keys = var.do_ssh_keys
+  ssh_keys = "${var.do_ssh_keys}"
 
   provisioner "remote-exec" {
     inline = ["true"]
