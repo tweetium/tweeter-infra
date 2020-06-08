b19cb8f255865cb5b0ed0a6d5c0a8fbbebd7fa0d
Darren Tsung <darren.tsung@gmail.com>
2020-06-07 18:55:07 -0700

    Change digitalocean snapshot name

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "digitalocean" {
 }
 
 data "digitalocean_image" "main" {
-  name = "main-with-consul"
+  name = "main"
 }
 
 # This ssh key may exist if you are managing multiple workspaces. To import this value, you
